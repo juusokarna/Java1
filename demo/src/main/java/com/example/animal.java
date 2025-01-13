41b3c83246e8354de1a4e2cebf45fdc2000a34c6
juusokarna <2002.juuso@gmail.com>
2025-01-13 21:24:08 +0200

    1st push

---
+++ demo/src/main/java/com/example/animal.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class animal {
+    String name;
+    String species;
+    int age;
+
+    public animal(String name, String species, int age) {
+        this.name = name;
+        this.species = species;
+    this.age = age;
+}
+}
