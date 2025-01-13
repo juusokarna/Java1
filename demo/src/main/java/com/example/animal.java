b5af5a839845b1ab09992b016aaf51acd785f754
juusokarna <2002.juuso@gmail.com>
2025-01-13 21:33:23 +0200

    2nd commit

--- demo/src/main/java/com/example/animal.java
+++ demo/src/main/java/com/example/animal.java
@@ -1,11 +1,11 @@
 package com.example;
 
-public class animal {
+public class Animal {
     String name;
     String species;
     int age;
 
-    public animal(String name, String species, int age) {
+    public Animal(String name, String species, int age) {
         this.name = name;
         this.species = species;
     this.age = age;
